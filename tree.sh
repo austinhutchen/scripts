40c95784b1db4c5e7cb5ab9cec06942293eaf45e
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-03 11:58:50 -0700

    fix

--- tree.sh
+++ tree.sh
@@ -2,7 +2,7 @@
 #LISTS THE FILE TREE FOUND AT INPUT DIRECTORY
 echo "Today is " `date`
 IN="/"
-read -p "Please enter the name of the directory to search, in format (ROOT)/{search} the_path
+read -p "Please the name of the directory to search, in format (ROOT)/{search}" the_path
 IN+=$the_path
 find $IN | sort | sed -e "s/[^-][^\/]*\//  |/g" -e "s/|\([^ ]\)/|-\1/"
 echo "\n DONE ... CLEANING UP!"
