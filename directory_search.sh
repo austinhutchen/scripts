f2668f08918fcac7dda67960eb241b500d3da4e8
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-03 09:11:17 -0700

    d

--- directory_search.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Today is " `date`
-echo -e "Enter the name of the directory to search, in format (ROOT)/{search}"
-IN="/"
-read the_path
-IN+=$the_path
-echo -e "Your path has the following files and folders: "
-ls $IN
