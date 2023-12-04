a41106cdfa2eeeea5beb8d5ebe80f842bf7856dd
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-04 12:07:15 -0700

    d

--- ping.sh
+++ ping.sh
@@ -9,7 +9,14 @@ do
     ifconfig $name |awk -v name=$name '/inet / {printf "%s: %s\n", name, $2; }'
 done
 
-read -p  "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.y): "
+read -p  "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.y): " SUBNET
 for IP in $(seq 100 254); do 
        ping -c 2 $SUBNET.$IP
 done
+read -p "Additionals? (y/n 1/0)" PROMPT
+if [ $PROMPT = 1 ]
+then
+  exec netstat -rn
+fi
+
+
