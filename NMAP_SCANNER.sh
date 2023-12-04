055cb9bbc2ace4b07d7e0407cc187b493d4417bd
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-04 12:29:21 -0700

    d

--- NMAP_SCANNER.sh
+++ NMAP_SCANNER.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
 #Simple pingsweep
-# Allows you to see listening posts ports on a local network and see wireless ports on a submask. 
-#
+# Allows you to see listening posts ports on a local network and see wireless ports on a submask.
+die() {
+  yell "$*"
+  exit 111
+}
+
 echo -e "YOU ARE CURRENTLY ON SUBNET @IP:"
-for name in $(ifconfig -l)
-do
-    ifconfig $name |awk -v name=$name '/inet / {printf "%s: %s\n", name, $2; }'
+for name in $(ifconfig -l); do
+  ifconfig $name | awk -v name=$name '/inet / {printf "%s: %s\n", name, $2; }'
 done
 
-read -p  "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.y): " SUBNET
-for IP in $(seq 100 254); do 
-       ping -c 2 $SUBNET.$IP
+read -p "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.BLAH): " SUBNET
+for IP in $(seq 1 254); do
+  if [ $($IP % 5) = 0 ]; then
+    read -p "continue or exit pings? 1/0" PROMPT
+    if [ $PROMPT = 0 ]; then
+      $(die)
+    fi
+    ping -c 2 $SUBNET.$IP
+  fi
 done
 read -p "Additionals? (y/n 1/0)" PROMPT
-if [ "$PROMPT" = "1" ]|| [ "$PROMPT" = "y" ]
-then
+if [ "$PROMPT" = "1" ] || [ "$PROMPT" = "y" ]; then
   exec netstat -rn
 fi
-
-
