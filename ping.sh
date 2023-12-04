9b15cb27605d66f14dfc8df768101160e2969fe9
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-04 11:57:57 -0700

    d

--- ping.sh
+++ ping.sh
@@ -4,7 +4,11 @@
 # Allows you to see active users on a given network (NMAP SCANNER)
 #
 echo -e "YOU ARE CURRENTLY ON SUBNET @IP:"
-ifconfig en0 |awk '/inet / {print $2; }'
+for name in $(ifconfig -l)
+do
+    ifconfig $name |awk -v name=$name '/inet / {printf "%s: %s\n", name, $2; }'
+done
+
 read -p  "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.y): "
 for IP in $(seq 100 254); do 
        ping -c 2 $SUBNET.$IP
