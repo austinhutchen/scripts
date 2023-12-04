5fb2342485701f152d8fee17c34dd09f9a3ecea9
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-03 18:10:36 -0700

    d

--- SYS_TREE.sh
+++ SYS_TREE.sh
@@ -6,7 +6,7 @@ echo -e "~~~~~~(1) SEARCH FROM ROOT: ~~~~~~ "
 ls $IN
 echo -e "~~~~~~(2) SEARCH FROM HOME: ~~~~~~ "
 ls $HOME
-echo -e "~~~~~~(3) SEARCH FOR A FILE: ~~~~~ \n"
+echo -e "~~~~~~(3) GLOBAL FILE SEARCH: ~~~~ \n"
 read -p "Please enter the number of your first choice, and the name of the item to search {FORMAT: 1|2 Directory OR 3 filename} " the_path
 NUMBER=$(echo $the_path | sed 's/[^0-9]*//g')
 echo -e "YOU CHOSE ${NUMBER} \n"
