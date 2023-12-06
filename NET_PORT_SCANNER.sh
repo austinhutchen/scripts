ded82f53eda3522f47bd379dffed244953c1c7d6
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-05 16:30:50 -0800

    d

--- NET_PORT_SCANNER.sh
+++ NET_PORT_SCANNER.sh
@@ -2,8 +2,7 @@
 # Simple pingsweep
 # Allows you to see listening posts ports on a local network and see wireless ports on a submask.
 finish() {
-  yell "$*"
-  exit 111
+  exit 
 }
 
 echo -e "YOU ARE CURRENTLY ON SUBNET @IP:"
@@ -12,15 +11,15 @@ for name in $(ifconfig -l); do
 done
 
 read -p "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.BLAH): " SUBNET
-for IP in $(seq 248 254); do
+for IP in $(seq 1 254); do
   read -p "continue or exit pings? 1/0" PROMPT
   if [ "$PROMPT" = 0 ]; then
     finish
   fi
-  ping -c 2 $SUBNET.$IP
+  ping -c 1 $SUBNET.$IP
 
 done
-read -p "Additionals? (y/n 1/0)" PROMPT
+read -p "PRINT MORE? (y/n | 1/0)" PROMPT
 if [ "$PROMPT" = "1" ] || [ "$PROMPT" = "y" ]; then
   exec netstat -rn
 fi
