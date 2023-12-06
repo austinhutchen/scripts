d68c716d55e3a77608458db348ea578d1b6108f6
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-12-05 20:04:40 -0800

    d

--- NET_PORT_SCANNER.sh
+++ NET_PORT_SCANNER.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Simple pingsweep
-# Allows you to see listening posts ports on a local network and see wireless ports on a submask.
+# Allows you to see listening posts ports on a submask / local network.
+# I USE THIS SCRIPT TO CHECK IF MY RASPBERRY PI IS ON 
 finish() {
-  exit 
+  exit
 }
 
 echo -e "YOU ARE CURRENTLY ON SUBNET @IP:"
@@ -11,10 +12,12 @@ for name in $(ifconfig -l); do
 done
 
 read -p "Please enter the subnet (first three digits in IP ADDRESS e.g. x.x.x.BLAH): " SUBNET
-for IP in $(seq 200 254); do
-  read -p "continue or exit pings? 1/0" PROMPT
-  if [ "$PROMPT" = 0 ]; then
-    finish
+for IP in $(seq 202 254); do
+  if [ "$IP % 5 " = 0 ]; then
+    read -p "continue or exit pings? 1/0" PROMPT
+    if [ "$PROMPT" = 0 ]; then
+      finish
+    fi
   fi
   ping -c 1 $SUBNET.$IP
 
